1d25de2ff2203f90cab77c8d50e01c23e6c47661
admin <1572903465@qq.com>
2020-10-08 10:18:57 +0800

    实验1_01实验完成版本 实验1_03初级完成版本 实验1_01 收到多条信息可储存 并同时查看

--- test01/test01_01/UdpServe.py
+++ test01/test01_01/UdpServe.py
@@ -1,19 +1,23 @@
 from socket import *
 import threading
 import time
+
 data=None
 addr=None
+data_arr = []
 
 def listen_Message():
     while True:
-        global data,addr                                #global全局变量
+        global data,addr                               #global全局变量
         data,addr = udp_serve.recvfrom(1024)
         if (data is not None):
             print("滴滴：主人，您收到了一条新消息")
-        #return data,addr
+            data_arr.append(data.decode("utf-8"))
 def view_Message():
     global data
-    print("您收到的新消息:",data.decode("utf-8"))
+    for i in range(len(data_arr)):
+        print("您收到的新消息:", data_arr[i])
+    data_arr.clear()
     data=None
 
 def send_Message():
@@ -26,19 +30,15 @@ if __name__ == '__main__':
     port = 8818
     udp_serve.bind((host,port))
     print("欢迎来到新新聊天室")
-    data = None
     th01 = threading.Thread(target=listen_Message)  #用线程解决了  recvfrom监听等待  和   input等待输入等待冲突的问题
     th01.start()
     while True:
-
-        # print(data.decode("utf-8"))
-        # print(addr)
         if(data is not None):
             while True:
                 time.sleep(0.5)
                 data_choice = input("请选择你的功能 按1接受消息，按2发送消息，按3接受消息并发送消息：\n")
                 if(data_choice=="1"):
-                    if(data is not None):
+                    if data_arr:
                         view_Message()
                     else:
                         print("对方还未发送新消息！")
@@ -46,7 +46,7 @@ if __name__ == '__main__':
                 elif(data_choice=="2"):
                     send_Message()
                 elif(data_choice=="3"):
-                    if (data is not None):
+                    if data_arr:
                         view_Message()
                     else:
                         print("对方还未发送新消息！")
--- test01/test01_01/UpdClient.py
+++ test01/test01_01/UpdClient.py
@@ -4,27 +4,28 @@ import time
 
 data = None
 addr = None
-
+data_arr =[]
 def send_Message():
     data_send = input("请输入你的信息:").encode("utf-8")
     upd_client.sendto(data_send, address)
 
 def receive_Message():
-    global data
-    print("您收到的新消息:",data.decode("utf-8"))
-    data=None
+    global data,data_arr
+    for i in range(len(data_arr)):
+        print("您收到的新消息:",data_arr[i])
+    data_arr.clear()
 
 def liten_Message():
-    global data, addr  # global全局变量
-    data,addr = upd_client.recvfrom(1024)
-    if (data is not None):
-        print("滴滴：主人，您收到了一条新消息")
-
+    while True:
+        global data, addr,data_arr  # global全局变量
+        data,addr = upd_client.recvfrom(1024)
+        if (data is not None):
+            print("滴滴：主人，您收到了一条新消息")
+            data_arr.append(data.decode("utf-8"))
 
 def fristSend_Message():
     data_send = input("请先跟对方打个招呼吧:").encode("utf-8")
     upd_client.sendto(data_send, address)
-
 
 if __name__ == '__main__':
     address=("127.0.0.2",8818)
@@ -39,7 +40,7 @@ if __name__ == '__main__':
                 time.sleep(0.5)
                 data_choice = input("请选择你的功能 按1接受消息，按2发送消息，按3接受消息并发送消息：\n")
                 if(data_choice == "1"):
-                    if (data is not None):
+                    if data_arr:
                         receive_Message()
                     else:
                         print("对方未发送新消息！")
@@ -47,7 +48,7 @@ if __name__ == '__main__':
                 elif(data_choice == "2"):
                     send_Message()
                 elif(data_choice == "3"):
-                    if(data is not None):
+                    if data_arr:
                         receive_Message()
                     else:
                         print("对方未发送新消息！")
